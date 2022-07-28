2a70be02bc172566fc18ca937951229d8ad9088d
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-07-28 10:02:04 +0300

    Add Store & currying

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { legacy_createStore } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const initialState = {
+  count: 0,
+  testProp: 77,
+};
+
+const reducer = (state = initialState, action) => {
+  return { ...state };
+};
+
+const store = legacy_createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
